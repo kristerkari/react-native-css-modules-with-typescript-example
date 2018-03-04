eec7892a2089b96143272d60f69f73e411a41ea5
krister <krister@sent.at>
2018-03-05 00:58:32 +0200

    Add react import

--- src/types/react-native-fontawesome.d.ts
+++ src/types/react-native-fontawesome.d.ts
@@ -1,4 +1,5 @@
 declare module "react-native-fontawesome" {
+  import React from "react";
   import { TextProperties } from "react-native";
   export const Icons: { [key: string]: string };
   const Icon: React.ComponentClass<TextProperties>;
