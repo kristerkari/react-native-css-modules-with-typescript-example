e7e70d0464396f8ccf7c7738c34bf9709be8db08
Krister Kari <krister@sent.at>
2021-07-09 01:33:09 +0300

    Fix linting

--- rn-transformer.js
+++ rn-transformer.js
@@ -2,11 +2,13 @@ var upstreamTransformer = require('metro-react-native-babel-transformer');
 var sassTransformer = require('react-native-sass-transformer');
 var postCSSTransformer = require('react-native-typed-postcss-transformer');
 
-module.exports.transform = function({src, filename, options}) {
+module.exports.transform = function ({src, filename, options}) {
   if (filename.endsWith('.scss')) {
     return sassTransformer
       .renderToCSS({src, filename, options})
-      .then(css => postCSSTransformer.transform({src: css, filename, options}));
+      .then((css) =>
+        postCSSTransformer.transform({src: css, filename, options}),
+      );
   } else if (filename.endsWith('.css')) {
     return postCSSTransformer.transform({src, filename, options});
   } else {
