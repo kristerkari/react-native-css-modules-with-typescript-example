346768964afa59a6120ade206505154706a2e359
Krister Kari <krister@sent.at>
2023-07-16 16:03:45 +0300

    Update React Native to 0.72

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+/**
+ * @format
+ */
+
 import 'react-native';
+import React from 'react';
+import App from '../src/App';
+
+// Note: import explicitly to use the types shiped with jest.
+import {it} from '@jest/globals';
+
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import App from '../src/App';
 
 it('renders correctly', () => {
   const tree = renderer.create(<App />);
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    ['@babel/plugin-transform-private-methods', {loose: true}],
     'react-native-classname-to-style',
     [
       'react-native-platform-specific-extensions',
